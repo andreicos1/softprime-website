38b0f13af4633d290e800848e9ad4f33b7894160
andrei-navys <andrei@navys.ai>
2026-02-23 18:16:04 +0200

    update footer; add legal pages

--- footer.js
+++ footer.js
@@ -6,7 +6,6 @@ document.querySelector('footer').innerHTML = `
         <p>All Rights Reserved.</p>
       </div>
       <div class="legal-info legal-details">
-        <p><strong>Headquarters:</strong> Mun. Iaşi, Str. Muşatini Nr. 23, Bl. V2, Sc. A, Et. 3, Ap. 14</p>
         <p>Reg. Com: J22/2071/04.07.2023</p>
         <p>CUI: 48430665 | EUID: ROONRC.J22/2071/2023</p>
       </div>
