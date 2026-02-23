9b19f20c4d77a97cfb9ebf58498bc38208512a28
andrei-navys <andrei@navys.ai>
2026-02-23 14:50:34 +0200

    add easy cals page

---
+++ footer.js
@@ -0,0 +1,15 @@
+document.querySelector('footer').innerHTML = `
+  <div class="container">
+    <div class="footer-content">
+      <div class="legal-info">
+        <p>© 2026 SOFTPRIME SOLUTIONS CONCEPT S.R.L.</p>
+        <p>All Rights Reserved.</p>
+      </div>
+      <div class="legal-info legal-details">
+        <p><strong>Headquarters:</strong> Mun. Iaşi, Str. Muşatini Nr. 23, Bl. V2, Sc. A, Et. 3, Ap. 14</p>
+        <p>Reg. Com: J22/2071/04.07.2023</p>
+        <p>CUI: 48430665 | EUID: ROONRC.J22/2071/2023</p>
+      </div>
+    </div>
+  </div>
+`;
